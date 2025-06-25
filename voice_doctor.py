1331d81ed3eb7fa84c97bb1563a4739a20314581
MUHAMMED FABIS NV <132933241+fabisnv@users.noreply.github.com>
2025-06-25 09:48:29 +0530

    Add files via upload

---
+++ voice_doctor.py
@@ -0,0 +1,35 @@
+import os
+from gtts import gTTS
+
+from playsound import playsound
+
+import subprocess
+import platform
+
+def text_to_speech_with_gtts(input_text, output_filepath):
+    language="en"
+
+    audioobj= gTTS(
+        text=input_text,
+        lang=language,
+        slow=False
+    )
+    audioobj.save(output_filepath)
+    os_name = platform.system()
+    try:
+        if os_name == "Darwin":  # macOS
+            subprocess.run(['afplay', output_filepath])
+        elif os_name == "Windows":  # Windows
+            playsound(output_filepath)
+        elif os_name == "Linux":  # Linux
+            subprocess.run(['aplay', output_filepath])  # Alternative: use 'mpg123' or 'ffplay'
+        else:
+            raise OSError("Unsupported operating system")
+    except Exception as e:
+        print(f"An error occurred while trying to play the audio: {e}")
+
+
+
+
+
+
